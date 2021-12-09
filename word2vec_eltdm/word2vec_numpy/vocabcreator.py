c160d0de364fbf210aad8c9aad973762f3774074
chloeskt <chloe.sekkat@ensae.fr>
2021-12-09 22:12:56 +0100

    change VocabCreator

--- word2vec_eltdm/word2vec_numpy/vocabcreator.py
+++ word2vec_eltdm/word2vec_numpy/vocabcreator.py
@@ -1,14 +1,12 @@
+from collections import Counter
 from typing import List, Tuple, Dict
 
 
 class VocabCreator:
     @staticmethod
     def create_vocab(tokens: List[str]) -> Tuple[Dict[str, int], Dict[int, str]]:
-        words_to_id = dict()
-        id_to_words = dict()
-
-        for i, token in enumerate(set(tokens)):
-            words_to_id[token] = i
-            id_to_words[i] = token
-
+        token_counts = Counter(tokens)
+        sorted_vocab = sorted(token_counts, key=token_counts.get, reverse=True)
+        id_to_words = {ii: token for ii, token in enumerate(sorted_vocab)}
+        words_to_id = {token: ii for ii, token in id_to_words.items()}
         return words_to_id, id_to_words
