dc2440c5282abb61b33f9172a85570449aed3a6b
chloeskt <chloe.sekkat@ensae.fr>
2021-12-11 13:50:03 +0100

    add interface for SDGHogwild

--- word2vec_eltdm/word2vec_numpy/optimizers.py
+++ word2vec_eltdm/word2vec_numpy/optimizers.py
@@ -12,3 +12,16 @@ class Optimizer:
         # dW1 = self.model.cache["X"].T @ (dy @ self.model.W2.T)
         self.model.W1 -= self.learning_rate * dW1
         self.model.W2 -= self.learning_rate * dW2
+
+
+class SGDHogwild:
+    """
+    SDG Hogwild from Hogwild!: A Lock-Free Approach to Parallelizing Stochastic Gradient Descent.
+    """
+
+    def __init__(self, model, learning_rate=5e-5):
+        self.model = model
+        self.learning_rate = learning_rate
+
+    def step(self, dW1, dW2):
+        raise NotImplementedError
