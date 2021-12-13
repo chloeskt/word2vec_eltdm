d0ccd815095009e6c704f47dda521fed5149d753
chloeskt <chloe.sekkat@ensae.fr>
2021-12-13 18:36:26 +0100

    add evaluation functions

--- word2vec_eltdm/word2vec_numpy/utils.py
+++ word2vec_eltdm/word2vec_numpy/utils.py
@@ -1,5 +1,10 @@
 from copy import deepcopy
+import random
+from typing import Dict
 
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.manifold import TSNE
 from tqdm.notebook import tqdm
 
 
@@ -15,9 +20,7 @@ def train(model, train_dataloader, criterion, optimizer):
         train_loss += loss
 
         if i % 1500 == 0:
-            print(
-                "Current Training Loss {:.6}".format(loss)
-            )
+            print("Current Training Loss {:.6}".format(loss))
 
     train_loss /= len(train_dataloader)
     return train_loss
@@ -49,3 +52,44 @@ def validate(model, dataloader, criterion):
     print("Validation Loss: ", validation_loss)
 
     return validation_loss
+
+
+def cosine_similarity(embeddings: np.array, len_vocab: int, nb_words: int = 20):
+    """
+    Returns the cosine similarity of randomly chosen `nb_words` words in the embedding matrix.
+    params:
+        embeddings: np.array
+            weights W1 corresponding to the embedding layer of our model
+    """
+    magnitudes = np.sqrt((embeddings ** 2).sum(axis=1))
+    examples = np.array(random.sample(range(len_vocab), nb_words))
+    examples_vectors = embeddings[examples]
+    similarities = (examples_vectors @ embeddings.T) / magnitudes
+    return examples, similarities
+
+
+def evaluate(embeddings: np.array, id_to_tokens: Dict[int, str], nb_words: int) -> None:
+    """
+    For `nb_words` words randomly chosen in the dataset, get the top 5 words according to their
+    cosine similarity (preview of word similarity) to evaluate qualitatively the model.
+    """
+    len_vocab = len(id_to_tokens)
+    examples, similarities = cosine_similarity(embeddings, len_vocab, nb_words)
+    closest_idxs = np.flip(np.argsort(similarities, axis=1)[:, -6:], axis=1)
+
+    for i, exemple_idx in enumerate(examples):
+        closest_words = [id_to_tokens[idx] for idx in closest_idxs[i]][1:]
+        print(id_to_tokens[exemple_idx] + " | " + ", ".join(closest_words))
+
+
+def visualization_tsne(
+    embeddings: np.array, id_to_tokens: Dict[int, str], nb_words: int = 400
+) -> None:
+    tsne = TSNE()
+    embed_tsne = tsne.fit_transform(embeddings[:nb_words, :])
+    fig, ax = plt.subplots(figsize=(16, 16))
+    for idx in range(nb_words):
+        plt.scatter(*embed_tsne[idx, :], color="steelblue")
+        plt.annotate(
+            id_to_tokens[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha=0.7
+        )
