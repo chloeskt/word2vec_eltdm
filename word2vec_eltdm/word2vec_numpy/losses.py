fix gradients in NSL

word2vec_eltdm/word2vec_numpy/losses.py
@@ -1,8 +1,7 @@
 import numpy as np
-from torch import nn
 
 
-class Loss(object):
+class Loss:
     def __init__(self):
         self.grad_history = []
 
@@ -36,16 +35,16 @@ class CrossEntropy(Loss):
         return grad
 
 
-class NegativeSamplingLoss(nn.Module):
+class NegativeSamplingLoss(Loss):
     def __init__(self):
         super().__init__()
 
     def __call__(self, model, input_vectors, output_vectors, noise_vectors, y):
         loss = self.forward(input_vectors, output_vectors, noise_vectors)
-        grad1, grad2 = self.backward(
+        grad1, grad2_pos, grad2_neg = self.backward(
             model, input_vectors, output_vectors, noise_vectors, y
         )
-        return loss, grad1, grad2
+        return loss, grad1, grad2_pos, grad2_neg
 
     def forward(self, input_vectors, output_vectors, noise_vectors):
         batch_size, embed_size = input_vectors.shape
@@ -65,7 +64,6 @@ class NegativeSamplingLoss(nn.Module):
         noise_loss = noise_loss.squeeze().sum(
             1
         )  # sum the losses over the sample of noise vectors
-
         # negate and sum correct and noisy log-sigmoid losses
         # return average batch loss
         return -(out_loss + noise_loss).mean()
@@ -83,27 +81,31 @@ class NegativeSamplingLoss(nn.Module):
         # Output vectors should be a batch of row vectors
         output_vectors = output_vectors.reshape(batch_size, 1, embed_size)
 
-        cj_h = self.sigmoid(output_vectors @ input_vectors).squeeze(axis=2)
-
-        cn_h = self.sigmoid(noise_vectors @ input_vectors).squeeze()
-
-        outs = np.concatenate([cj_h, cn_h], axis=1)
-
-        # compute prediction error
-        outs[:, -1] -= 1
-        outs = np.expand_dims(outs, axis=1)
-
-        # gradient wrt to W1
-        W1_y = model.W1[y, :].reshape(batch_size, 1, embed_size)
-        W1_noise = model.W1[model.cache["noise_words"], :].reshape(
-            batch_size, model.cache["n_samples"], embed_size
+        sigmoid_context = (
+            self.sigmoid(output_vectors @ input_vectors).squeeze(axis=2) - 1
+        ).squeeze()
+        product_context = np.multiply(
+            output_vectors.squeeze(), sigmoid_context[:, None]
         )
-        W1_batch = np.concatenate([W1_y, W1_noise], axis=1)
-        grad_W1 = (outs @ W1_batch).squeeze()
 
-        # gradient wrt to W2
-        outs = np.moveaxis(outs, [0, 1, 2], [0, 2, 1])
-        input_vectors = np.moveaxis(input_vectors, [0, 1, 2], [0, 2, 1])
-        grad_W2 = outs @ input_vectors
+        sigmoid_noise = self.sigmoid(-noise_vectors @ input_vectors)
+        sigmoid_noise -= np.ones(sigmoid_noise.shape)
 
-        return grad_W1, grad_W2
+        context_noise = np.multiply(noise_vectors, sigmoid_noise)
+        context_noise = context_noise.sum(axis=1)
+
+        # gradient wrt W1
+        grad_W1 = product_context - context_noise
+
+        # gradient wrt context words
+        grad_W2_positive = np.multiply(
+            input_vectors.squeeze(), sigmoid_context[:, None]
+        )
+
+        # gradient wrt negative words (one gradient for each negative word and for each
+        # context word)
+        grad_W2_negative = np.negative(
+            np.multiply(input_vectors.reshape(batch_size, 1, embed_size), sigmoid_noise)
+        )
+
+        return grad_W1, grad_W2_positive, grad_W2_negative

word2vec_eltdm/word2vec_numpy/models.py
@@ -17,12 +17,10 @@ class SimpleWord2Vec(Network):
     def __init__(
         self,
         len_vocab: int,
-        hidden_size: int = 500,
         embedding_size: int = 300,
     ):
         super(SimpleWord2Vec, self).__init__("SimpleWord2Vec")
         self.len_vocab = len_vocab
-        self.hidden_size = hidden_size
         self.embedding_size = embedding_size
         self.cache = {}
         self.best_val_loss = None
@@ -87,8 +85,11 @@ class SimpleWord2Vec(Network):
         model = {self.model_name: self}
         if not os.path.exists(directory):
             os.makedirs(directory)
-        with open(directory + "/" + self.model_name + "_" + str(self.best_val_loss) + ".p", "wb") as file:
-            print('Saving model')
+        with open(
+            directory + "/" + self.model_name + "_" + str(self.best_val_loss) + ".p",
+            "wb",
+        ) as file:
+            print("Saving model")
             pickle.dump(model, file)
 
 
@@ -100,13 +101,11 @@ class NegWord2Vec:
     def __init__(
         self,
         len_vocab: int,
-        hidden_size: int = 500,
         embedding_size: int = 300,
         noise_dist: np.array = None,
-        best_val_loss = None
+        best_val_loss=None,
     ):
         self.len_vocab = len_vocab
-        self.hidden_size = hidden_size
         self.embedding_size = embedding_size
         self.noise_dist = noise_dist
         self.cache = {}
@@ -129,14 +128,24 @@ class NegWord2Vec:
             self.W2 = W2
 
         else:
-            self.W1 = np.random.uniform(
-                low=-1,
-                high=1,
+            # self.W1 = np.random.uniform(
+            #     low=-1,
+            #     high=1,
+            #     size=(self.len_vocab, self.embedding_size),
+            # )
+            # self.W2 = np.random.uniform(
+            #     low=-1,
+            #     high=1,
+            #     size=(self.len_vocab, self.embedding_size),
+            # )
+            self.W1 = np.random.normal(
+                loc=0.0,
+                scale=1 / np.sqrt(self.embedding_size),
                 size=(self.len_vocab, self.embedding_size),
             )
-            self.W2 = np.random.uniform(
-                low=-1,
-                high=1,
+            self.W2 = np.random.normal(
+                loc=0.0,
+                scale=1 / np.sqrt(self.embedding_size),
                 size=(self.len_vocab, self.embedding_size),
             )
 
@@ -192,6 +201,9 @@ class NegWord2Vec:
         model = {self.model_name: self}
         if not os.path.exists(directory):
             os.makedirs(directory)
-        with open(directory + "/" + self.model_name + "_" + str(self.best_val_loss) + ".p", "wb") as file:
-            print('Saving model')
+        with open(
+            directory + "/" + self.model_name + "_" + str(self.best_val_loss) + ".p",
+            "wb",
+        ) as file:
+            print("Saving model")
             pickle.dump(model, file)

word2vec_eltdm/word2vec_numpy/optimizers.py
@@ -78,28 +78,22 @@ class OptimizeNSL(Optimizer):
         self.method = method
         self.iterations = 0
 
-    def step(self, dW1, dW2):
+    def step(self, dW1, dW2_pos, dW2_neg):
         batch_size, embed_size = dW1.shape
         # update W1 weights
         X = self.model.cache["X"]
         self.model.W1[X.squeeze(), :] -= self.learning_rate * dW1
 
-        # update W2 weights
+        # update W2 weights for positive samples
         y = self.model.cache["y"]
-        W2_y = np.moveaxis(self.model.W2[y, :], [0, 1, 2], [1, 0, 2])
-        W2_noise = self.model.W1[self.model.cache["noise_words"], :].reshape(
-            batch_size, self.model.cache["n_samples"], embed_size
-        )
+        self.model.W2[y.squeeze(), :] -= self.learning_rate * dW2_pos
 
-        W2 = np.concatenate([W2_y, W2_noise], axis=1)
-        update_W2 = W2 - self.learning_rate * dW2
-
-        y = y.reshape(-1, 1)
-        noise_matrix = self.model.cache["noise_words"].reshape(
-            batch_size, self.model.cache["n_samples"]
+        # update W2 weights for negative samples
+        dW2_neg = dW2_neg.reshape(
+            batch_size * self.model.cache["n_samples"], embed_size
         )
-        M = np.concatenate([y, noise_matrix], axis=1)
-        for i in range(M.shape[0]):
-            self.model.W2[M[i], :] = update_W2[i]
+        self.model.W2[self.model.cache["noise_words"], :] -= (
+            self.learning_rate * dW2_neg
+        )
 
         self.iterations += 1